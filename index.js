098b849b3f7c2a26b46f705c4d69fc63063714a6
유으뜸 <www.goolge.com>
2024-06-10 10:36:35 +0900

    name

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 class person {
-  constructor(){
+  constructor() {
     this._name = "";
     this._age = 0;
     this._job = "";
+  }
+  set name(value) {
+    this._name = value;
+  }
+  get name() {
+    return this._name;
   }
 }
