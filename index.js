0d18a1377ad56a2c91d8ecdd5da4d94da83a05b9
유으뜸 <www.goolge.com>
2024-06-10 10:47:36 +0900

    최종 마무리

--- index.js
+++ index.js
@@ -36,19 +36,21 @@
 const createPerson = (callback) => {
   let person = {};
   callback(person, setName);
-}
+};
 
 const setName = (person, callback) => {
   person.name = "유으뜸";
   callback(person, setAge);
-}
+};
 
 const setAge = (person, callback) => {
   person.age = "11";
   callback(person, setJob);
-}
+};
 
 const setJob = (person, callback) => {
   person.job = "수강생";
   console.log(person);
-}
+};
+
+createPerson((person, callback) => callback(person, setName));
